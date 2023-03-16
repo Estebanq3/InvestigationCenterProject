c8e4584314834f570f3158449e2210f38b0e9863
Estebanq3 <estebanq52@gmail.com>
2023-03-16 14:39:27 -0600

    Add project to GitHub

---
+++ ResearchRepository/Database_ResearchRepository/StoredProcedures/GetIdByNameTheses.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[GetIdByNameTheses]
+(	
+@name varchar(300) = '',
+@publicationDate datetime
+)
+AS
+BEGIN
+SELECT * from dbo.Thesis t1 WHERE t1.Name = @name and t1.PublicationDate = @publicationDate
+END
---
+++ ResearchRepository/Database_ResearchRepository/Tables/PublicationPartOfTesis.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[PublicationPartOfTesis]
+(
+	[PublicationId] VARCHAR(50) NOT NULL FOREIGN KEY REFERENCES Publication(Id), 
+    [ThesisId] INT NOT NULL FOREIGN KEY REFERENCES Thesis(Id),
+	PRIMARY KEY ([PublicationId],[ThesisId])
+)
